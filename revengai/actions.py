24bf7b7807d28c1703335cd2cb473fdaad79cbe7
Yannick Tournier <tournier.yannick@gmail.com>
2024-03-27 12:11:35 +0100

    update action list

--- revengai/actions.py
+++ revengai/actions.py
@@ -1,27 +1,29 @@
 # -*- coding: utf-8 -*-
 
 from os.path import exists, basename
-from requests import HTTPError, Response
 
-from idc import get_input_file_path
-from reait.api import RE_upload, RE_analyse, RE_status
+import idc
+from requests import HTTPError, Response, post
+
+from reait.api import RE_upload, RE_analyse, RE_status, reveng_req
 from revengai.features.auto_analyze import AutoAnalysisDialog
 
 from revengai.gui.dialog import Dialog
 from revengai.manager import RevEngState
 from revengai.features.function_simularity import FunctionSimularityDialog
+from revengai.misc.utils import IDAUtils
 from revengai.wizard.wizard import RevEngSetupWizard
 
 
 def setup_wizard(state: RevEngState) -> None:
-    RevEngSetupWizard(state).exec()
+    RevEngSetupWizard(state).exec_()
 
 
 def upload_binary(state: RevEngState) -> None:
     if not state.config.is_valid():
         setup_wizard(state)
     else:
-        path = get_input_file_path()
+        path = idc.get_input_file_path()
 
         if exists(path):
             try:
@@ -37,7 +39,7 @@ def check_analyze(state: RevEngState) -> None:
     if not state.config.is_valid():
         setup_wizard(state)
     else:
-        path = get_input_file_path()
+        path = idc.get_input_file_path()
 
         if exists(path):
             try:
@@ -58,19 +60,41 @@ def auto_analyze(state: RevEngState) -> None:
     if not state.config.is_valid():
         setup_wizard(state)
     else:
-        path = get_input_file_path()
+        path = idc.get_input_file_path()
 
         if exists(path):
             dialog = AutoAnalysisDialog(state, path)
             dialog.exec_()
 
 
-def rename_functions(state: RevEngState) -> None:
+def rename_function(state: RevEngState) -> None:
     if not state.config.is_valid():
         setup_wizard(state)
     else:
-        path = get_input_file_path()
+        path = idc.get_input_file_path()
 
         if exists(path):
             dialog = FunctionSimularityDialog(state, path)
             dialog.exec_()
+
+
+def explain_function(state: RevEngState) -> None:
+    if not state.config.is_valid():
+        setup_wizard(state)
+    else:
+        try:
+            # pseudo_code = IDAUtils.disasm_func(idc.here())
+
+            pseudo_code = IDAUtils.decompile_func(idc.here())
+
+            if len(pseudo_code) > 0:
+                res: Response = reveng_req(post, "explain", data=pseudo_code)
+
+                print(res.text)
+                res.raise_for_status()
+
+                # IDAUtils.set_comment(idc.here(), res.json()["explanation"])
+        except HTTPError as e:
+            if "error" in e.response.json():
+                Dialog.showError("",
+                                 f"Error getting function explanation: {e.response.json()['error']}")
