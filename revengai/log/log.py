8965cb62a715291db42b678c9d1131c0e5d7e9be
Yannick Tournier <yannick@reveng.ai>
2024-04-16 19:35:39 +0200

    cleanup

--- revengai/log/log.py
+++ revengai/log/log.py
@@ -1,25 +1,23 @@
 # -*- coding: utf-8 -*-
 
-import logging
-import logging.config
-
 from os import makedirs
 from pathlib import Path
 from shutil import rmtree
+from logging.config import fileConfig
+
 
 LOG_CONFIG_FILENAME = "log.ini"
 LOG_FILENAME = "reai.log"
 
 
-def clear_logs(log_dir):
+def clear_logs(log_dir: str) -> None:
     rmtree(log_dir, ignore_errors=True)
 
 
-def configure_loggers(log_dir):
+def configure_loggers(log_dir: str) -> None:
     makedirs(log_dir, exist_ok=True)
 
-    log_main_file = Path(log_dir) / LOG_FILENAME
-    log_conf_file = Path(__file__).resolve().parent / LOG_CONFIG_FILENAME
+    log_main_file: Path = Path(log_dir) / LOG_FILENAME
+    log_conf_file: Path = Path(__file__).resolve().parent / LOG_CONFIG_FILENAME
 
-    logging.config.fileConfig(log_conf_file.as_posix(),
-                              defaults={"default_log_filename": log_main_file.as_posix()})
+    fileConfig(fname=log_conf_file.as_posix(), defaults={"default_log_filename": log_main_file.as_posix()})
