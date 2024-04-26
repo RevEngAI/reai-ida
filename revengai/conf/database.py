e1ca13ee29df88cbd33f3328c8161ea920bfc7d6
Yannick Tournier <yannick@reveng.ai>
2024-04-26 18:40:46 +0200

    close cursor after every use

--- revengai/conf/database.py
+++ revengai/conf/database.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import logging
+from contextlib import closing
 
 from os import makedirs
 from os.path import join, basename
@@ -42,19 +43,18 @@ class RevEngDatabase(object):
 
     def create_tables(self) -> None:
         try:
-            cursor = self.conn.cursor()
+            with closing(self.conn.cursor()) as cursor:
+                cursor.execute("""
+                CREATE TABLE IF NOT EXISTS upload(
+                id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+                sha_256_hash TEXT NOT NULL UNIQUE, name TEXT);
+                """)
 
-            cursor.execute("""
-                        CREATE TABLE IF NOT EXISTS upload(
-                        id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-                        sha_256_hash TEXT NOT NULL UNIQUE, name TEXT);
-                        """)
-
-            cursor.execute("""
-                        CREATE TABLE IF NOT EXISTS analysis(
-                        id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-                        sha_256_hash TEXT NOT NULL, binary_id INTEGER NOT NULL UNIQUE, status TEXT, submitted TEXT);
-                        """)
+                cursor.execute("""
+                CREATE TABLE IF NOT EXISTS analysis(
+                id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+                sha_256_hash TEXT NOT NULL, binary_id INTEGER NOT NULL UNIQUE, status TEXT, submitted TEXT);
+                """)
         except Error as e:
             logger.error("Error creating tables to local database. %s", e)
         finally:
@@ -62,10 +62,9 @@ class RevEngDatabase(object):
 
     def drop_tables(self) -> None:
         try:
-            cursor = self.conn.cursor()
-
-            for table in ["upload", "analysis"]:
-                cursor.execute(f"DROP TABLE {table}")
+            with closing(self.conn.cursor()) as cursor:
+                for table in ["upload", "analysis"]:
+                    cursor.execute(f"DROP TABLE {table}")
         except Error as e:
             logger.error("Error dropping tables from local database. %s", e)
         finally:
@@ -73,8 +72,9 @@ class RevEngDatabase(object):
 
     def add_upload(self, fpath: str, sha_256_hash: str) -> None:
         try:
-            self.conn.cursor().execute("INSERT OR REPLACE INTO upload(name, sha_256_hash) VALUES(?, ?)",
-                                       (basename(fpath), sha_256_hash,))
+            with closing(self.conn.cursor()) as cursor:
+                cursor.execute("INSERT OR REPLACE INTO upload(name, sha_256_hash) VALUES(?, ?)",
+                               (basename(fpath), sha_256_hash,))
         except Error as e:
             logger.error("Error adding upload into local database for hash: %s. %s", sha_256_hash, e)
         finally:
@@ -82,9 +82,9 @@ class RevEngDatabase(object):
 
     def delete_upload(self, sha_256_hash: str) -> None:
         try:
-            for table in ["upload", "analysis"]:
-                self.conn.cursor().execute(f"DELETE FROM {table} WHERE sha_256_hash = ?",
-                                           (sha_256_hash,))
+            with closing(self.conn.cursor()) as cursor:
+                for table in ["upload", "analysis"]:
+                    cursor.execute(f"DELETE FROM {table} WHERE sha_256_hash = ?", (sha_256_hash,))
         except Error as e:
             logger.error("Error deleting upload from local database for hash: %s. %s", sha_256_hash, e)
         finally:
@@ -92,20 +92,20 @@ class RevEngDatabase(object):
 
     def get_last_analysis(self, sha_256_hash: str) -> int:
         try:
-            cursor = self.conn.cursor()
+            with closing(self.conn.cursor()) as cursor:
+                cursor.execute("SELECT binary_id FROM analysis WHERE sha_256_hash = ? ORDER BY binary_id DESC LIMIT 1",
+                               (sha_256_hash,))
 
-            cursor.execute("SELECT binary_id FROM analysis WHERE sha_256_hash = ? ORDER BY binary_id DESC LIMIT 1",
-                           (sha_256_hash,))
-
-            result = cursor.fetchone()
-            return result[0] if result and len(result) > 0 else 0
+                result = cursor.fetchone()
+                return result[0] if result and len(result) > 0 else 0
         except Error as e:
             logger.error("Error getting last analysis for hash: %s. %s", sha_256_hash, e)
 
     def add_analysis(self, sha_256_hash: str, bid: int, status: str = "", submitted: str = "") -> None:
         try:
-            self.conn.cursor().execute("INSERT OR REPLACE INTO analysis(sha_256_hash, binary_id, status, submitted) VALUES(?, ?, ?, ?)",
-                                       (sha_256_hash, bid, status, submitted,))
+            with closing(self.conn.cursor()) as cursor:
+                cursor.execute("INSERT OR REPLACE INTO analysis(sha_256_hash, binary_id, status, submitted) VALUES(?, ?, ?, ?)",
+                               (sha_256_hash, bid, status, submitted,))
         except Error as e:
             logger.error("Error adding analysis for bid: %d, status: %s, hash: %s. %s",
                          bid, status, sha_256_hash, e)
@@ -114,8 +114,8 @@ class RevEngDatabase(object):
 
     def update_analysis(self, bid: int, status: str) -> None:
         try:
-            self.conn.cursor().execute("UPDATE analysis SET status = ? WHERE binary_id = ?",
-                                       (status, bid,))
+            with closing(self.conn.cursor()) as cursor:
+                cursor.execute("UPDATE analysis SET status = ? WHERE binary_id = ?", (status, bid,))
         except Error as e:
             logger.error("Error updating analysis for bid: %d, status: %s. %s", bid, status, e)
         finally:
@@ -123,8 +123,8 @@ class RevEngDatabase(object):
 
     def delete_analysis(self, bid: int) -> None:
         try:
-            self.conn.cursor().execute("DELETE FROM analysis WHERE binary_id = ?",
-                                       (bid,))
+            with closing(self.conn.cursor()) as cursor:
+                cursor.execute("DELETE FROM analysis WHERE binary_id = ?", (bid,))
         except Error as e:
             logger.error("Error deleting analysis from database for bid: %d. %s", bid, e)
         finally:
