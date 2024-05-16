a126b948d46459b5d6af32d236639d6089d3ac17
Yannick Tournier <yannick@reveng.ai>
2024-05-16 18:22:27 +0200

    rollback health check

--- revengai/api/__init__.py
+++ revengai/api/__init__.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
-
 from __future__ import annotations
 
 from os.path import basename
-from requests import get, post, Response, HTTPError
+from requests import get, post, Response
 
-from reait.api import reveng_req, re_binary_id, re_bid_search
+from reait.api import reveng_req, re_binary_id, re_bid_search, ReaitError
 
 
 def RE_collections_count(scope: str = "PUBLIC") -> Response:
@@ -29,10 +28,12 @@ def RE_analyze_functions(fpath: str, binary_id: int = 0) -> Response:
     bin_id = re_binary_id(fpath)
     bid = re_bid_search(bin_id) if binary_id == 0 else binary_id
 
-    if bid == -1:
-        raise HTTPError(f"No matches found for hash: {bin_id}")
+    end_point = f"v1/analyse/functions/{bid}"
 
-    res: Response = reveng_req(get, f"v1/analyse/functions/{bid}")
+    if bid == -1:
+        raise ReaitError(f"No matches found for hash: {bin_id}", end_point)
+
+    res: Response = reveng_req(get, end_point)
 
     res.raise_for_status()
     return res
