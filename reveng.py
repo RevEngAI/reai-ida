a15b03b5d80722648bfef297c71af2b90db296ed
Yannick Tournier <yannick@reveng.ai>
2024-06-11 15:33:15 +0200

    wip

--- reveng.py
+++ reveng.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import logging
 
-from idaapi import plugin_t, PLUGIN_FIX, PLUGIN_SKIP, PLUGIN_OK, PLUGIN_KEEP, get_kernel_version
+from idaapi import plugin_t, PLUGIN_SKIP, PLUGIN_OK, PLUGIN_KEEP, IDA_SDK_VERSION
 
 from revengai.manager import RevEngState
 
@@ -11,7 +11,8 @@ logger = logging.getLogger("REAI")
 
 class RevEngPlugin(plugin_t):
     # Variables required by IDA
-    flags = PLUGIN_FIX  # Normal plugin
+    flags = 0  # Normal plugin
+    wanted_hotkey = "Ctrl-Shift-R"
     wanted_name = "RevEng.AI Toolkit"
     help = f"Configure IDA plugin for {wanted_name}"
     comment = f"AI-assisted reverse engineering from {wanted_name}"
@@ -22,20 +23,22 @@ class RevEngPlugin(plugin_t):
         self.initialized = False
         self.state = RevEngState()
 
-    def init(self):
-        kv = get_kernel_version().split(".")
-        if int(kv[0]) < 8:
-            logger.info("%s need IDA version >= 8.0 => skipping...", self.wanted_name)
+    def init(self) -> int:
+        """
+        Called when the plugin is initialised.
+        """
+        if IDA_SDK_VERSION < 800:
+            logger.warning("%s support 8.X IDA => skipping...", self.wanted_name)
             return PLUGIN_SKIP
 
-        logger.info("%s initialized", self.wanted_name)
+        logger.info("%s plugin starts", self.wanted_name)
 
         if self.state.config.auto_start:
             self.run()
             return PLUGIN_KEEP
         return PLUGIN_OK
 
-    def reload_plugin(self):
+    def reload_plugin(self) -> bool:
         if self.initialized:
             self.term()
 
@@ -43,11 +46,18 @@ class RevEngPlugin(plugin_t):
 
         self.state.start_plugin()
         self.initialized = True
+        return True
 
-    def run(self, _=None):
-        self.reload_plugin()
+    def run(self, _=None) -> bool:
+        """
+        Called when the plugin is invoked.
+        """
+        return self.reload_plugin()
 
-    def term(self):
+    def term(self) -> None:
+        """
+        Called when the plugin is unloaded.
+        """
         logger.info("Terminating %s...", self.wanted_name)
         if self.state is not None:
             self.state.stop_plugin()
