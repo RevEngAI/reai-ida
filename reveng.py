4927e5e429f0307e3b0a87962b9e4f93c853dc3d
Yannick Tournier <yannick@reveng.ai>
2024-06-24 15:07:03 +0200

    add comments

--- reveng.py
+++ reveng.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 import logging
 
-from idc import get_inf_attr, INF_FILETYPE, FT_ELF, FT_PE, FT_MACHO, FT_EXE, FT_BIN, \
-    INF_APPTYPE, APPT_LIBRARY, APPT_PROGRAM
-from idaapi import execute_ui_requests, plugin_t, IDA_SDK_VERSION, \
-    PLUGIN_SKIP, PLUGIN_OK, PLUGIN_KEEP, PLUGIN_HIDE
+from idc import get_inf_attr, APPT_LIBRARY, APPT_PROGRAM, INF_APPTYPE, INF_FILETYPE, \
+    FT_ELF, FT_PE, FT_MACHO, FT_EXE, FT_BIN
+from idaapi import execute_ui_requests, plugin_t, IDA_SDK_VERSION, PLUGIN_SKIP, PLUGIN_OK, PLUGIN_KEEP, PLUGIN_HIDE
 
 from revengai.gui import Requests
 
@@ -25,14 +24,19 @@ except ImportError:
 
     from idc import msg
 
-    msg("[!] RevEng.AI Toolkit requires Python module reait.\n")
+    msg("[!] RevEng.AI Toolkit requires the Python module called 'reait'.\n")
 
 
 logger = logging.getLogger("REAI")
 
 
 class RevEngPlugin(plugin_t):
+    """
+    Define the plugin class itself which is returned by the PLUGIN_ENTRY method
+    that scriptable plugins use to be recognized within IDA
+    """
     # Variables required by IDA
+    # Use the HIDE to avoid the entry in Edit/Plugins since this plugin's run() method has no functionality.
     flags = 0 if IDA_SDK_VERSION > 810 else PLUGIN_HIDE
     wanted_hotkey = ""
     wanted_name = "RevEng.AI Toolkit"
@@ -52,7 +56,7 @@ class RevEngPlugin(plugin_t):
         if IDA_SDK_VERSION < 800:
             logger.warning("%s support 8.X IDA => skipping...", self.wanted_name)
             return PLUGIN_SKIP
-        elif get_inf_attr(INF_APPTYPE) not in (APPT_LIBRARY, APPT_PROGRAM) and \
+        elif get_inf_attr(INF_APPTYPE) not in (APPT_LIBRARY, APPT_PROGRAM,) and \
                 get_inf_attr(INF_FILETYPE) not in (FT_BIN, FT_PE, FT_ELF, FT_EXE, FT_MACHO,):
             logger.warning("%s supports PE, ELF, RAW, EXE, DLL and Mach-O file types => skipping...", self.wanted_name)
             return PLUGIN_SKIP
@@ -61,6 +65,8 @@ class RevEngPlugin(plugin_t):
 
         if self.state.config.auto_start:
             self.run()
+
+            # Return KEEP instead of OK to keep the plugin loaded since it registers callback actions and hotkeys
             return PLUGIN_KEEP
         return PLUGIN_OK
 
@@ -91,12 +97,13 @@ class RevEngPlugin(plugin_t):
         self.initialized = False
 
 
+# The PLUGIN_ENTRY method is what IDA calls when scriptable plugins are loaded.
+# It needs to return a plugin of type idaapi.plugin_t.
 def PLUGIN_ENTRY():
     global required_modules_loaded
 
     if required_modules_loaded:
         return RevEngPlugin()
 
-    execute_ui_requests((Requests.MsgBox(RevEngPlugin.wanted_name,
-                                         f"[{RevEngPlugin.wanted_name}] Unable to load all required modules."),))
+    execute_ui_requests((Requests.MsgBox(RevEngPlugin.wanted_name, "Unable to load all the required modules.", -1),))
     return None
