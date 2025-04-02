d3dbfeb00ec9ace98d08437d8b0e468413799817
lloydlabs <lloyd@reveng.ai>
2025-04-02 19:20:02 +0100

    feat: features/auto_unstrip - sync local function addresses with REAI functions

--- revengai/features/auto_unstrip.py
+++ revengai/features/auto_unstrip.py
@@ -13,13 +13,21 @@ from revengai.manager import RevEngState
 logger = logging.getLogger("REAI")
 
 
+@staticmethod
+def _divide_chunks(data: list, n: int = 50) -> list:
+    for idx in range(0, len(data), n):
+        yield data[idx : idx + n]
+
+
 class AutoUnstrip:
     def __init__(self, state: RevEngState):
         self.state = state
         self.base_addr = get_imagebase()
         self.analysed_functions = {}
+        self.functions = self._get_all_functions()
 
         self._get_analysed_functions()
+        self.function_ids = self._get_sync_analysed_ids_local()
 
     def _get_all_functions(self) -> list:
         functions = []
@@ -58,8 +66,23 @@ class AutoUnstrip:
                 ),
             )
 
-    def get_all_auto_unstrip_matches(self, max_workers=1,
-                                     distance=0.09999999999999998):
+    def _get_sync_analysed_ids_local(self) -> list:
+        function_ids = []
+
+        for idx, func in enumerate(self._functions):
+            idx += 1
+
+            function_id = self.analyzed_functions.get(func["start_addr"], None)
+
+            if function_id:
+                function_ids.append(function_id)
+
+        return function_ids
+
+    def _get_all_auto_unstrip_matches(
+        self, max_workers=1, distance=0.09999999999999998
+    ):
+
         with ThreadPoolExecutor(
             max_workers=max_workers, thread_name_prefix="reai-batch"
         ) as executor:
@@ -69,12 +92,17 @@ class AutoUnstrip:
                     return RE_nearest_symbols_batch(
                         function_ids=chunk,
                         distance=distance,
-                        debug_enabled=True
+                        debug_enabled=True,
                     ).json()["function_matches"]
                 except Exception as ex:
                     return ex
 
-            futures = {executor.submit(worker, chunk): chunk}
+            futures = {
+                executor.submit(worker, chunk): chunk
+                for chunk in _divide_chunks(
+                    self.function_ids, self.state.project_cfg.get("chunk_size")
+                )
+            }
 
             for future, chunk in futures.items():
                 res = future.result() if not future.cancelled() else None
@@ -85,8 +113,7 @@ class AutoUnstrip:
                     func_addr = next(
                         (
                             func_addr
-                            for func_addr, func_id
-                            in self.analysed_functions.items()
+                            for func_addr, func_id in self.analysed_functions.items()
                             if symbol["origin_function_id"] == func_id
                         ),
                         None,
