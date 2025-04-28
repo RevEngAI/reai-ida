e131747b317778886c54c688309009a5d9f151f3
dzonerzy <dzonerzy@gmail.com>
2025-04-28 17:35:21 +0200

    remove useless logs

--- revengai/features/auto_analyze.py
+++ revengai/features/auto_analyze.py
@@ -886,35 +886,20 @@ class AutoAnalysisDialog(BaseDialog):
 
             # include binaries too
             try:
-                logger.info(
-                    "Searching for binaries with '%s'", query or "N/A"
-                )
-
                 res: dict = RE_binaries_search(
                     query=query,
                     page=1,
                     page_size=1024,
                 ).json()
-                logger.info(f"res: {res}")
-
                 result_binaries = res.get("data", {}).get("results", [])
             except HTTPError as e:
-                # TODO: this must be changed when the API is fixed
                 resp = e.response.json()
-                errors = resp.get("errors", [])
-                if len(errors) == 0:
-                    detail = resp.get("detail", "Unknown error")
-                    if detail == "At least one filter must be provided":
-                        result_binaries = []
+                errors = resp.get("errors", [{}])
+                error_code = errors[0].get("code", "unknown")
+                if error_code == "missing":
+                    result_binaries = []
                 else:
-                    if len(errors) >= 1:
-                        error_code = errors[0].get("code", "unknown")
-                        if error_code == "missing":
-                            result_binaries = []
-                        else:
-                            raise e
-                    else:
-                        raise e
+                    raise e
 
             logger.info(f"Found {len(result_binaries)} binaries")
 
