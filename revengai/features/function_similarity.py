cdf2985f8bd9f68d02d35f284db963bd6d9294f2
Anisio Santos <39890990+anisiosts@users.noreply.github.com>
2025-06-17 12:21:19 -0300

    fix: remove comment

--- revengai/features/function_similarity.py
+++ revengai/features/function_similarity.py
@@ -277,7 +277,7 @@ class FunctionSimilarityDialog(BaseDialog):
                 function["function_id"] = function_id
                 try:
                     logger.info(f"Getting name score for {distance}")
-                    name_score = RE_name_score([{"function_id": function_id, "function_name": nnfn}]).json()["data"]Add commentMore actions
+                    name_score = RE_name_score([{"function_id": function_id, "function_name": nnfn}]).json()["data"]
                     confidence = name_score[0]["box_plot"]["average"]
                     if confidence < (100 - (distance * 100)):
                         logger.info(f"Skipping {nnfn} because it's not similar enough to {nnfn}")
