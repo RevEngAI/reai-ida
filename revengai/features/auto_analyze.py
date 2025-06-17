4e2c4f4738709b19139b6d34b85b125829cfda97
dzonerzy <dzonerzy@gmail.com>
2025-06-17 18:14:18 +0200

    fix #PLU-143 the new is_in_exec_segment check segment permission rather than name

--- revengai/features/auto_analyze.py
+++ revengai/features/auto_analyze.py
@@ -122,7 +122,9 @@ class AutoAnalysisDialog(BaseDialog):
 
         for func_ea in Functions():
             start_addr = idc.get_func_attr(func_ea, idc.FUNCATTR_START)
-            if IDAUtils.is_in_valid_segment(start_addr):
+            # why should we care if the function is in a valid segment?
+            # as long as IDA identifies it as a function, we MUST analyze it
+            if IDAUtils.is_in_exec_segment(start_addr):
                 # add only if the function name starts with sub_
                 name = IDAUtils.get_demangled_func_name(func_ea)
                 if name.startswith("sub_"):
@@ -384,9 +386,11 @@ class AutoAnalysisDialog(BaseDialog):
         )
 
     def _start_analysis(self) -> None:
+        logger.info("Starting auto analysis…")
         inthread(self._auto_analysis)
 
     def _auto_analysis(self) -> None:
+        logger.info("Auto analysis started")
         try:
             inmain(show_wait_box, "Getting results…")
 
@@ -405,6 +409,11 @@ class AutoAnalysisDialog(BaseDialog):
             if not self.analyzed_functions or \
                     len(self.analyzed_functions) == 0:
                 self._get_analyze_functions()
+
+            logger.info(
+                "Found %d functions to analyze",
+                len(self._functions)
+            )
 
             resultsData = []
             function_ids = []
@@ -750,6 +759,13 @@ class AutoAnalysisDialog(BaseDialog):
             )
         except RequestException as e:
             logger.error("An unexpected error has occurred. %s", e)
+        except Exception as e:
+            logger.error("An unexpected error has occurred. %s", e)
+            Dialog.showError(
+                "Auto Analysis",
+                f"Unexpected error: {e}"
+            )
+            inmain(idaapi.warning, f"Unexpected error: {e}")
         finally:
             inmain(hide_wait_box)
             inmain(self.ui.fetchResultsButton.setEnabled, True)
